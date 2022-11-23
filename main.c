35fb498d30d70981806fbde61c86fe5bdb4fcf6d
Keorse <91177455+Keorse@users.noreply.github.com>
2022-11-23 15:20:24 +0100

    lala

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+typedef struct avion{
+    int id;
+    int cat_av;//1 av de ligne    2 av affaire     3 av léger
+    int is_flying;// 1 pour un avion en vol    0 pour un avion au sol
+    int nb_passengers;
+}avion;
+
+typedef struct piste{
+    int num_piste;
+    float longueur;
+    int cat_piste;//1 petite  2 Moyenne   3 petite
+    int max_await;
+    avion* liste_av;
+}piste;
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+piste init_piste(int num_piste, float longueur, int cat_piste, int max_await){
+    
+};
